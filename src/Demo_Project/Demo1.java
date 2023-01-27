22a88a137ba6c7fd0c4848278f667fb7a9206d7b
anand <anandmali1890@gmail.com>
2023-01-27 19:17:02 +0530

    "Thhis Is First Commit"

---
+++ src/Demo_Project/Demo1.java
@@ -0,0 +1,15 @@
+package Demo_Project;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+
+		int age = 19;
+		if(age == 19) {
+			System.out.println("You Are The Adult");
+		}else {
+			System.out.println("You Are Not Adult");
+		}
+	}
+
+}
